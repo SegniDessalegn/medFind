171a3df888807ff1b50b0569181296b7e9ec69e2
habtamu <habtetsegaye19@gmail..com>
2022-01-23 00:04:44 +0300

    watchlist service added

--- src/main/java/com/gis/medfind/repository/WatchListRepository.java
+++ src/main/java/com/gis/medfind/repository/WatchListRepository.java
@@ -2,10 +2,13 @@ package com.gis.medfind.repository;
 import com.gis.medfind.entity.WatchList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WatchListRepository extends JpaRepository<WatchList, Long> {
+    @Query(value ="SELECT wl FROM watch_list wl WHERE wl.fk_owner =:id", nativeQuery=true)
+    public WatchList findWatchListByUserId(@Param("id") Long userId);
 }
-
 
