09029da767d43de075f83a273a26ae17495a8195
amha kindu <amhaznif@gmail.com>
2022-01-23 10:33:12 +0300

    Initial entities and services

---
+++ src/main/java/com/gis/medfind/MedfindApplication.java
@@ -0,0 +1,13 @@
+package com.gis.medfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedfindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedfindApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gis/medfind/entity/Role.java
@@ -0,0 +1,35 @@
+package com.gis.medfind.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    @ManyToMany
+    @JoinTable(
+        name = "privileges_roles", 
+        joinColumns = @JoinColumn(
+          name = "role_id", referencedColumnName = "id"), 
+        inverseJoinColumns = @JoinColumn(
+          name = "privilege_id", referencedColumnName = "id"))
+    private List<Privilege> privileges;
+}
